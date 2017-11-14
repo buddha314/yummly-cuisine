580da6d8017fbacb56192f9cff774545a0957679
buddha314 <buddha_314@yahoo.com>
2017-11-14 13:41:22 -0800

    now counting intersection and crystal size

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,8 +6,17 @@ CREATE TABLE r.yummly_crystals_raw (
 , original_crystals text
 , crystals text
 );
-
 // \copy r.yummly_crystals_raw from 'output.txt' with csv header delimiter E'\t'
+
+DROP TABLE IF EXISTS r.yummly_recipe;
+CREATE TABLE r.yummly_recipe (
+  cuisine text
+, recipe_id int
+, ingredient text
+);
+
+// \copy r.yummly_recipe from 'app/data/yummly.txt' with csv header delimiter E'\t'
+
 
 DROP TABLE IF EXISTS r.yummly_crystals_tall;
 CREATE TABLE r.yummly_crystals_tall AS
@@ -34,10 +43,35 @@ CREATE TABLE r.yummly_inflation (
 , crystal_id int
 , entropy real
 , inflation real
+, intersection_size int
 , symdiff_size int
 );
 
-// \copy r.yummly_inflation from 'inflations_20171106.txt' with csv header delimiter E'\t'
+// \copy r.yummly_inflation from 'results/inflation_20171114.txt' with csv header delimiter E'\t'
+
+DROP TABLE IF EXISTS r.yummly_rank;
+CREATE TABLE r.yummly_rank AS
+SELECT
+  crystal_id
+, recipe_id
+, (entropy-inflation) AS err
+, intersection_size
+, symdiff_size
+, DENSE_RANK() OVER (PARTITION BY crystal_id ORDER BY entropy-inflation) AS r
+FROM r.yummly_inflation
+;
+
+DROP TABLE IF EXISTS r.yummly_rank_recipe;
+CREATE TABLE r.yummly_rank_recipe AS
+SELECT
+  crystal_id
+, recipe_id
+, (entropy-inflation) AS err
+, symdiff_size
+, DENSE_RANK() OVER (PARTITION BY recipe_id ORDER BY entropy-inflation) AS r
+FROM r.yummly_inflation
+;
+
 
 DROP TABLE IF EXISTS r.yummly_crystal_fits;
 CREATE TABLE r.yummly_crystal_fits AS
